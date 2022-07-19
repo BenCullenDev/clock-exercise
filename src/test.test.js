478c6d44a9f3704001bb4215bca5f6804da057b3
Ben Cullen <benjaminjcullen1@gmail.com>
2022-07-19 09:52:40 +0100

    tidied up unit test

--- src/test.test.js
+++ src/test.test.js
@@ -3,10 +3,6 @@ import App from "../src/components/App.js";
 
 test("clock", () => {
 	render(<App />);
-
-	//select the elements you want to interact with
 	const clock = screen.getByTestId("clock");
-
-	//assert the expected result
 	expect(clock).toBeInTheDocument;
 });
